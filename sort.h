e187c8ae07485d30578e37725747cb18ee96c59d
Julie Tolve <9919@holbertonstudents.com>
2024-12-11 09:06:35 +0100

    betty header

--- sort.h
+++ sort.h
@@ -14,9 +14,9 @@
 
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /* Prototypes of the functions */
@@ -28,4 +28,4 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
-#endif 
+#endif
